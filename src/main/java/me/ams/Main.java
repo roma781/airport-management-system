96fcae3ae99b1e6498eae408510dd936eb3d71e0
Ramazan Seilbekov <ramazanseilbekk@gmail.com>
2025-01-29 10:37:08 +0500

    Using singleton instead of creating new object

--- src/main/java/me/ams/Main.java
+++ src/main/java/me/ams/Main.java
@@ -4,12 +4,11 @@ import me.ams.database.Database;
 
 public class Main {
     public static void main(String[] args) {
-        Database db = new Database();
-        db.setHost("jdbc:postgresql://localhost:5432");
-        db.setUsername("postgres");
-        db.setPassword("underfan111");
-        db.setDbName("airport-management-system");
+        Database.getInstance().setHost("jdbc:postgresql://localhost:5432");
+        Database.getInstance().setUsername("postgres");
+        Database.getInstance().setPassword("underfan111");
+        Database.getInstance().setDbName("airport-management-system");
 
-        System.out.println(db.getConnection());
+        System.out.println(Database.getInstance().getConnection());
     }
 }
--- src/main/java/me/ams/database/Database.java
+++ src/main/java/me/ams/database/Database.java
@@ -14,6 +14,8 @@ public class Database {
 
     private Connection connection;
 
+    private Database() {}
+
     public static Database getInstance() {
         if (instance == null) { instance = new Database(); }
         return instance;
